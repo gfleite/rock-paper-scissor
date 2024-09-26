ff84404fbfeea9558b49e6e2bf1b5fc86756399e
Gabriel <62269094+gfleite@users.noreply.github.com>
2024-09-26 02:13:24 -0300

    Add function playRound and edit other functions to return values instead of only logging them

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
-let ComputerChoice, HumanChoice, computerScore, humanScore;
+let computerChoice, humanChoice, computerScore, humanScore;
 
 computerScore = 0;
 humanScore = 0;
 
-function getComputerChoice(ComputerChoice){
-    if(ComputerChoice >= 0.33 && ComputerChoice< 0.66) {
-        return console.log("Rock");
-    } else if (ComputerChoice < 0.33) {
-        return console.log("Paper");
+function getComputerChoice(computerChoice){
+    if(computerChoice >= 0.33 && computerChoice< 0.66) {
+        computerChoice = "Rock";
+        console.log(computerChoice);
+        return computerChoice;
+    } else if (computerChoice < 0.33) {
+        computerChoice = "Paper";
+        console.log(computerChoice);
+        return computerChoice;
     } else {
-        return console.log("Scissors");
+        computerChoice = "Scissors";
+        console.log(computerChoice);
+        return computerChoice;
     }
 }
 
-function getHumanChoice(HumanChoice){
-    if(HumanChoice == 1) {
-        return console.log("Rock");
-    } else if (HumanChoice == 2) {
-        return console.log("Paper");
-    } else if (HumanChoice == 3) {
-        return console.log("Scissors");
+function getHumanChoice(humanChoice){
+    if(humanChoice == 1) {
+        humanChoice = "Rock";
+        console.log(humanChoice);
+        return humanChoice;
+    } else if (humanChoice == 2) {
+        humanChoice = "Paper";
+        console.log(humanChoice);
+        return humanChoice;
+    } else if (humanChoice == 3) {
+        humanChoice = "Scissors";
+        console.log(humanChoice);
+        return humanChoice;
     } else {
         console.log("Please choose a valid option");
         getHumanChoice(prompt("Type 1 for Rock, 2 for Paper, 3 for Scissors"));
     }
-
 }
 
-getComputerChoice(Math.random());
-getHumanChoice(prompt("Type 1 for Rock, 2 for Paper, 3 for Scissors"));
+function playRound(computerChoice, humanChoice){
+        if(computerChoice === humanChoice){
+            console.log(computerChoice);
+            console.log(humanChoice);
+            console.log("That's a tie!");
+        } else if (
+            (computerChoice === "Rock" && humanChoice === "Paper") ||
+            (computerChoice === "Paper" && humanChoice === "Scissors") ||
+            (computerChoice === "Scissors" && humanChoice === "Rock")
+        ) {
+            console.log("You won!");
+        } else {
+            console.log("You lost!");
+        }
+}
+
+computerChoice = getComputerChoice(Math.random());
+humanChoice = getHumanChoice(prompt("Type 1 for Rock, 2 for Paper, 3 for Scissors"));
+playRound(computerChoice, humanChoice);
